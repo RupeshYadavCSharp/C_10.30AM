341ff207e9425e2330ee9a8780508279d1a62568
RupeshYadavCSharp <81669954+RupeshYadavCSharp@users.noreply.github.com>
2023-11-30 11:00:23 +0530

    s
    switch

---
+++ Rupesh Sir Code/switchdemo.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main()
+{
+    char choice;
+    float plates,billamt;
+    printf("D.Dosa(100)\nI.Idli(60)\nW.Wada(40)\nEnter choice\n");
+    scanf("%c",&choice);
+
+    switch (choice)
+    {
+        case 'D':
+            printf("-------------Dosa-----------------\n");
+            printf("how many plates ?\n");
+            scanf("%f",&plates);
+            billamt = plates * 100;
+            printf("please pay rs %0.2f\n",billamt);
+            break;
+        case 'I':
+          printf("-------------IDLI-----------------\n");
+            printf("how many plates ?\n");
+            scanf("%f",&plates);
+            billamt = plates * 60;
+            printf("please pay rs %0.2f\n",billamt);
+            break;
+        case 'W':
+          printf("-------------Wada-----------------\n");
+            printf("how many plates ?\n");
+            scanf("%f",&plates);
+            billamt = plates * 40;
+            printf("please pay rs %0.2f\n",billamt);
+            break;
+        default:
+            printf("invalid choice \n");
+            break;
+
+    }
+
+
+
+
+    return 0;
+}
