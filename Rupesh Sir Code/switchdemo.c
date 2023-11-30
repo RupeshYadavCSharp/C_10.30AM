23990ba092b6c2984d51975a33ef5eda09691ed1
RupeshYadavCSharp <81669954+RupeshYadavCSharp@users.noreply.github.com>
2023-11-30 11:04:36 +0530

    added multiple choice

--- Rupesh Sir Code/switchdemo.c
+++ Rupesh Sir Code/switchdemo.c
@@ -10,13 +10,16 @@ int main()
     switch (choice)
     {
         case 'D':
+        case 'd':
             printf("-------------Dosa-----------------\n");
             printf("how many plates ?\n");
             scanf("%f",&plates);
             billamt = plates * 100;
             printf("please pay rs %0.2f\n",billamt);
             break;
+        
         case 'I':
+        case 'i':
           printf("-------------IDLI-----------------\n");
             printf("how many plates ?\n");
             scanf("%f",&plates);
@@ -24,6 +27,7 @@ int main()
             printf("please pay rs %0.2f\n",billamt);
             break;
         case 'W':
+        case 'w':
           printf("-------------Wada-----------------\n");
             printf("how many plates ?\n");
             scanf("%f",&plates);
