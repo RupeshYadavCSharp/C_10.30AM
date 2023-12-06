08afc32bcf0dedd59308afa89d4d3ddb8399718a
RupeshYadavCSharp <81669954+RupeshYadavCSharp@users.noreply.github.com>
2023-12-06 11:59:15 +0530

    pattern

--- Rupesh Sir Code/forloopnesting.c
+++ Rupesh Sir Code/forloopnesting.c
@@ -2,14 +2,25 @@
 
 int main()
 {
+    int k = 1;
     for(int i = 1;i<=5;i++)
     {
-        for(int j = 1; j<=5;j++)
+        for(int j = 1; j<=i;j++)
         {
-            printf("* ");
+            printf("%d ",k);
+            k++;
         }
-
         printf("\n");
 
     }
 }
+
+/*
+1
+2 3
+4 5 6
+7 8 9 10 
+11 12 13 14 15 
+
+
+*/
